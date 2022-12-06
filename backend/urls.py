90bc810f249ec8faee69cdd96f214792fcc301d6
Daikku <vadess2013@mail.ru>
2022-12-06 22:14:37 +0300

    Updated static and media root

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.http import HttpResponse
-
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("nftapes.urls")),
     path("healthcheck/", lambda r: HttpResponse()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            serve, {'document_root': settings.STATIC_ROOT}),
+    ]
